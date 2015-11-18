2b6bea21ebcaccb09455f6a087e493344868e69c
cm898946 <cm898946@gmail.com>
2015-11-18 10:04:00 -0500

    Integrated runAll into Main.m

--- Main.m
+++ Main.m
@@ -11,16 +11,16 @@ radius = (.75*volume/pi)^(1/3);                     %m
 crossSectionalArea = pi*radius^2;                   %m^2
 gravity = 9.80551;                                  %m/s^2
 length = .3;                                        %m
-angle = pi/2;                                      %degrees
+initV = 0;                                          %m/s
+initAng = -pi/2;                                    %rad
 acceleration = 0;                                   %m/s^2
-velocity = 0;                                       %m/s
 cableWeightperLength = .179;                        %kg/m
 cableWeight = cableWeightperLength * length;        %kg
 
 %Forces
 gravitational = mass*gravity;                       %N
-centrifugal = (mass*velocity^2)/radius;             %N
-tension = centrifugal + gravitational*cos(angle);   %N
+centrifugal = (mass*initV^2)/radius;             %N
+tension = centrifugal + gravitational*cos(0);   %N
 totalx = 0;
 totaly = 0;
 
@@ -31,24 +31,22 @@ dragCoefficient = 0.5;
 airDensity = 1.225;                                 %kg/m^3
 
 %Friction Forces
-drag = .5*airDensity*dragCoefficient*crossSectionalArea*velocity^2; %N
+drag = .5*airDensity*dragCoefficient*crossSectionalArea*initV^2; %N
 hinge = tension*bearingCoefficientFriction;  %N
 
-[T,U] = ode45(@angV,[0,5],angle);
+initParams = [initAng;initV];
+[T,U] = ode45(@move,[0,10000],initParams);
 
-plot(T,U,'b');
+plot(T,U(:,1));
 %Equations of motion
     
-    function res = angV(t,theta)
-        wangs2 = velocity + angAcc(theta);
-        display(wangs2);
-        res = wangs2;
-    end
-
-    function res = angAcc(theta)
-        wangs = -(gravity/length)*sin(theta);
-        %display(wangs);
-        res = wangs;
+    function res = move(t,params)
+    angle = params(1);
+    velocity  = params(2);
+    dAng = velocity;
+    dangV = -(gravity/length)*sin(angle) - .5*(velocity*length)^2;
+   
+    res = [dAng;dangV];
     end
 
 end
